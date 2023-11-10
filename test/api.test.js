dc51ec0d07157ec89336cfcac933d9137382fcef
Veselin Atanasov <veselin.atanasov@opencode.com>
2023-11-10 13:43:09 +0200

    add unit test

--- test/api.test.js
+++ test/api.test.js
@@ -5,3 +5,23 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe('Testing Book API', () => {
+  let bookId;
+
+  it('should create a new book using POST', (done) => {
+    const book = { id: 1, title: 'test book', author: 'test author' };
+    chai
+      .request(server)
+      .post('/books')
+      .send(book)
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('id');
+        expect(res.body).to.have.property('title');
+        expect(res.body).to.have.property('author');
+        bookId = res.body.id;
+        done();
+      });
+  });
+});
